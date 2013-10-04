0a5f40f73a85e10fd691a0db518e7ba06e187046
Michal Staniaszek <m.staniaszek@gmail.com>
2013-10-04 19:40:27 +0200

    Static board cost calculation and testing

--- src/board/StaticBoard.java
+++ src/board/StaticBoard.java
@@ -1,24 +1,26 @@
 package board;
 
 import java.awt.Point;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 import utilities.SokobanUtil;
+import utilities.SokobanUtil.Action;
+import utilities.WeightedPoint;
 
 public class StaticBoard {
 
 	private static StaticBoard INSTANCE = null;
-
 	
 	/** Warning, grid[y][x] is the value at the point (x,y)*/
-	public final Symbol[][] grid;
-	/** Deprecated, use costMap.getKeySet() instead) */
-	@Deprecated
+	public final Symbol[][] grid; 
 	public final List<Point> goals;
-	public static Map<Point, Map<Point, Integer>> costMap;
+    public final Map<Point, Map<Point, Integer> > goalDistanceCost;
 	
 	public static void init(Symbol[][] grid, List<Point> goals) {
 		INSTANCE = new StaticBoard(grid, goals);
@@ -32,20 +34,62 @@ public class StaticBoard {
 	private StaticBoard(Symbol[][] grid, List<Point> goals) {
 		this.grid = grid;
 		this.goals = Collections.unmodifiableList(goals);
-		this.costMap = Collections.unmodifiableMap(StaticBoard.computeCosts(grid, goals));
+        this.goalDistanceCost = new HashMap<>();
+        generatePathCosts();
 	}
-	
-	private static Map<Point, Map<Point, Integer>> computeCosts(
-			Symbol[][] grid, List<Point> goals) {
-		Map<Point, Map<Point, Integer>> output = new HashMap<>();
-		for (Point goal : goals) {
-			Board b = new Board();
-			// b.setAvailablePosition();
+    
+    /**
+     * Fills the goalDistanceCost map with a map for each point on the board. Each point
+     * maps to another map, which contains a mapping from goal points to integers
+     * representing the cost of getting to the given point from the given goal.
+     */
+    private void generatePathCosts(){
+        for (Point goal : goals) {
+            Queue<WeightedPoint> open = new LinkedList<>();
+            List<Point> closed = new ArrayList<>();
+            
+            WeightedPoint wp = new WeightedPoint(goal, 0);
+            open.add(wp);
+            while (!open.isEmpty()) { // while there are unexpanded points
+                WeightedPoint next = open.remove();
+                closed.add(next.point);
+                // Only consider walkable positions
+                if (get(next.point).isWalkable){
+                    // Make sure the hashmap for this point is initialised
+                    Map<Point, Integer> thisPoint = goalDistanceCost.get(next.point);
+                    if (thisPoint == null){
+                        thisPoint = new HashMap<>();
+                        goalDistanceCost.put(next.point, thisPoint);
+                    }
+                    
+                    // Put the goal node being checked along with the cost of getting to
+                    // this position from that goal into the map for this point.
+                    thisPoint.put(goal, next.cost);
+                    closed.add(next.point);
+                    // expand the neighbours of this point and add them to the
+                    // open list if they have not yet been visited.
+                    List<WeightedPoint> neighbours = expandPoint(next);
+                    for (WeightedPoint neighbour : neighbours) {
+                        if (!closed.contains(neighbour.point)){
+                            open.add(neighbour);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private List<WeightedPoint> expandPoint(WeightedPoint p){
+        List<WeightedPoint> freeNeighbours = new ArrayList<>();
+		for (Action a : Action.values()) {
+			WeightedPoint neighbour = new WeightedPoint(SokobanUtil.applyActionToPoint(a, p.point), p.cost + 1);
+			if (get(neighbour.point).isWalkable) {
+				freeNeighbours.add(neighbour);
+			}
 		}
-		// 
-		return output;
-	}
-
+		return freeNeighbours;
+    }
+	
 	public Symbol get(Point point) {
 		return get(point.x, point.y);
 	}
--- test/board/StaticBoardTest.java
+++ test/board/StaticBoardTest.java
@@ -1,22 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package board;
 
-import static org.junit.Assert.assertTrue;
-
 import java.awt.Point;
-import java.util.HashMap;
 import java.util.Map;
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+        
 import utilities.TestUtil;
 
+/**
+ *
+ * @author michal
+ */
 public class StaticBoardTest {
+    
+    public StaticBoardTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
 
-	@Test
-	public void testComputeCosts() {
-		final String TEST_FILE = "readTest1.map";
-        TestUtil.initBoard(TEST_FILE);
-        assertTrue(new HashMap<Point, Map<Point, Integer>>().equals(StaticBoard.costMap) );
-	}
+    @Test
+    public void testInit() {
+        Board t = TestUtil.initBoard("testPathCost.map");
+        Map<Point, Map<Point, Integer> > costs = StaticBoard.getInstance().goalDistanceCost;
+        for (Point point : StaticBoard.getInstance().goals) {
+            Map<Point, Integer> gp = costs.get(point);
+            assertTrue(gp.containsKey(point));
+            if (point == new Point(1,3)){
+                assertEquals(gp.get(new Point(1,3)).intValue(), 0);
+                assertEquals(gp.get(new Point(3,1)).intValue(), 4);
+            } else if (point == new Point(3,1)){
+                assertEquals(gp.get(new Point(1,3)).intValue(), 4);
+                assertEquals(gp.get(new Point(3,1)).intValue(), 0);
+            }
+        }
+    }
 
 }
