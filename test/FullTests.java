39d2092fc6f6ce970896a041ad64149bdd0b4626
ndossougbe <fake@fake.fake>
2013-10-16 17:02:13 +0200

    Test to run the 100 example maps

--- test/FullTests.java
+++ test/FullTests.java
@@ -1,8 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 import utilities.SokobanUtil;
+import utilities.SokobanUtil.Action;
 import utilities.TestUtil;
 import board.Board;
+
+import static org.junit.Assert.*;
 
 
 public class FullTests {
@@ -15,10 +21,22 @@ public class FullTests {
 	
 	@Test
 	public void test100Maps() {
-		for (int i = 0; i < 100; ++i) {
-			Board start = TestUtil.initBoard("../test100/test000.in");
-			System.out.print(SokobanUtil.actionListAsString(Main.solveBoardBidirectional(start)));
+		final int startMap = 0;
+		final int nbTests = 100 - startMap;
+		
+		
+		Boolean[] results = new Boolean[nbTests];
+		for (int i = startMap; i < nbTests; ++i) {
+			Board start = TestUtil.initBoard(String.format("../test100/test%03d.in", i));
+			Board copy = new Board(start);
+			List<Action> moves = Main.solveBoardBidirectional(start);
+			System.out.println("Board " + i + ": " + SokobanUtil.actionListAsString(moves));
+			results[i] = copy.applyActionChained(moves, true).isSolved();
+			
 		}
+		Boolean[] expected = new Boolean[nbTests];
+		Arrays.fill(expected, true);
+		assertArrayEquals(expected, results);
 	}
 
 }
