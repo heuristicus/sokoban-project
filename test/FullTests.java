7c255ef3c6bbab05148e39a17669269edbc1d28c
ndossougbe <fake@fake.fake>
2013-10-16 17:36:49 +0200

    Added timing and exception handling to the test100 runner

--- test/FullTests.java
+++ test/FullTests.java
@@ -1,5 +1,9 @@
+import static org.junit.Assert.assertArrayEquals;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.junit.Test;
 
@@ -7,8 +11,6 @@ import utilities.SokobanUtil;
 import utilities.SokobanUtil.Action;
 import utilities.TestUtil;
 import board.Board;
-
-import static org.junit.Assert.*;
 
 
 public class FullTests {
@@ -22,21 +24,39 @@ public class FullTests {
 	@Test
 	public void test100Maps() {
 		final int startMap = 0;
-		final int nbTests = 100 - startMap;
+		final int endMap = 10;
 		
-		
+		final int nbTests = endMap - startMap;
 		Boolean[] results = new Boolean[nbTests];
-		for (int i = startMap; i < nbTests; ++i) {
-			Board start = TestUtil.initBoard(String.format("../test100/test%03d.in", i));
+		Map<Integer, String> times = new TreeMap<Integer, String>();
+		for (int i = 0; i < nbTests; ++i) {
+			Board start = TestUtil.initBoard(String.format("../test100/test%03d.in", i + startMap));
 			Board copy = new Board(start);
-			List<Action> moves = Main.solveBoardBidirectional(start);
-			System.out.println("Board " + i + ": " + SokobanUtil.actionListAsString(moves));
-			results[i] = copy.applyActionChained(moves, true).isSolved();
+			List<Action> moves = null;
+			
+
+			
+			try {
+				long startTime = System.nanoTime();
+				moves = Main.solveBoardBidirectional(start);				
+				long endTime = System.nanoTime();
+				times.put(i + startMap, String.format("%.2g",(endTime - startTime) / 10e9));
+			} catch(Exception e) {
+				System.err.println("Exception on map " + (i + startMap) );
+				e.printStackTrace();
+			}
+			if (moves != null) {
+				System.out.println("Board " + (i + startMap) + ": " + SokobanUtil.actionListAsString(moves));
+				results[i] = copy.applyActionChained(moves, true).isSolved();				
+			} else {
+				results[i] = false;
+			}
 			
 		}
 		Boolean[] expected = new Boolean[nbTests];
 		Arrays.fill(expected, true);
 		assertArrayEquals(expected, results);
+		System.out.println(times);
 	}
 
 }
